3c2df5c75aad204c6b0fdd5b3de3668ef3e77b63
PranjalPise <74958152+PranjalPise@users.noreply.github.com>
2020-11-27 14:14:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obsta
 var score;
 var gameOverImg,restartImg
 var jumpSound , checkPointSound, dieSound;
-localStorage["HighestScore"];
+localStorage["HighestScore"]=0;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
